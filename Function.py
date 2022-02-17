18b7e544e559c4ae6d55d473ff79486c54fc8705
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-17 14:29:18 +0100

    failded

---
+++ Function.py
@@ -0,0 +1,21 @@
+from geopy.geocoders import Nominatim
+
+def add_df_to_lan_lat_geojson(df, properties):
+    geojson = {'type':'FeatureCollection', 'features':[]}
+    geo = Nominatim(user_agent="not-my-application")
+    for _, row in df.iterrows():
+        try:
+            corr = geo.geocode(row["pays"])
+        except:
+            continue
+        feature = {'type':'Feature',
+                   "color": "blue",
+                   'properties':{},
+                   'geometry':{'type':'Point',
+                               'coordinates':[]}}
+        feature['geometry']['coordinates'] = [corr.latitude,corr.latitude]
+        for prop in properties:
+            feature['properties'][prop] = row[prop]
+        geojson['features'].append(feature)
+    return geojson
+
---
+++ row_to_csv.py
@@ -0,0 +1,9 @@
+from geopy.geocoders import Nominatim
+import pandas as pd
+from Function import add_df_to_lan_lat_geojson
+
+properties = ["pays","cluster_km"]
+src = pd.read_csv("./data/data_with_clusters.csv")
+print("Wating assiging the contry coordinate...")
+geoJson = add_df_to_lan_lat_geojson(src, properties)
+print(geoJson)
