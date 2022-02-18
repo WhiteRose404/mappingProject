de99772cb8e33d7f29af7e425de340b610960983
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-18 01:02:57 +0100

    fixed a bug

--- Function.py
+++ Function.py
@@ -1,21 +1,14 @@
 from geopy.geocoders import Nominatim
 
 def add_df_to_lan_lat_geojson(df, properties):
-    geojson = {'type':'FeatureCollection', 'features':[]}
-    geo = Nominatim(user_agent="not-my-application")
-    for _, row in df.iterrows():
-        try:
-            corr = geo.geocode(row["pays"])
-        except:
-            continue
-        feature = {'type':'Feature',
-                   "color": "blue",
-                   'properties':{},
-                   'geometry':{'type':'Point',
-                               'coordinates':[]}}
-        feature['geometry']['coordinates'] = [corr.latitude,corr.latitude]
-        for prop in properties:
-            feature['properties'][prop] = row[prop]
-        geojson['features'].append(feature)
-    return geojson
+	geojson = {'type':'FeatureCollection', 'features':[]}
+	geo = Nominatim(user_agent="not-my-application")
+	for _, row in df.iterrows():
+		feature = {'type':'Feature','color': 'blue','properties':{},'geometry':{'type':'Point','coordinates':[]}}
+		corr = geo.geocode(row["pays"])
+		feature['geometry']['coordinates'] = [corr.latitude,corr.longitude]
+		for prop in properties:
+			feature['properties'][prop] = row[prop]
+			geojson['features'].append(feature)
+		return geojson
 
