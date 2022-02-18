09e2c0a8c56bec109a1cb6e4f193792726331f12
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-18 01:14:30 +0100

    fixed

--- Function.py
+++ Function.py
@@ -4,16 +4,13 @@ def add_df_to_lan_lat_geojson(df, properties):
     geojson = {'type':'FeatureCollection', 'features':[]}
     geo = Nominatim(user_agent="not-my-application")
     for _, row in df.iterrows():
-        try:
-            corr = geo.geocode(row["pays"])
-        except:
-            continue
         feature = {'type':'Feature',
                    "color": "blue",
                    'properties':{},
                    'geometry':{'type':'Point',
                                'coordinates':[]}}
-        feature['geometry']['coordinates'] = [corr.latitude,corr.latitude]
+        corr = geo.geocode(row["pays"])
+        feature['geometry']['coordinates'] = [corr.latitude,corr.longitude]
         for prop in properties:
             feature['properties'][prop] = row[prop]
         geojson['features'].append(feature)
