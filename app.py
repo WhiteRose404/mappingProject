5045a9c50c642f616036fc7a66b2040c08c6f319
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-19 20:49:12 +0100

    DONE

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from pathlib import Path
 import json
 
 folder = Path.cwd() / "data"
-df = pd.read_csv(folder / "Eng_data_with_clusters_f.csv")
+property = ["pays","cluster_km_2","cluster_km","cluster_km_5"]
+
+ind = int(input("chose Your cluster \n" + ", ".join(property[1:]) + " :"))
+print("Ploting...")
+df = pd.read_csv(folder / "ENG_data_with_clusters_f.csv")[property]
 with open(folder / "countries.geojson") as f:
     data = json.load(f)
-    fig = px.choropleth_mapbox( df, geojson=data, color="cluster_km",
+    fig = px.choropleth_mapbox( df, geojson=data, color=property[ind],
                                 locations="pays",featureidkey="properties.ADMIN",
-                            mapbox_style="carto-positron")
+                                mapbox_style="carto-positron",hover_data=["pays","cluster_km_2","cluster_km","cluster_km_5"])
+    print("Done")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     fig.show()
