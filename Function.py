f3ea773a0104c976bc1d13eb727e4477ece71279
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-02-19 02:14:19 +0100

    fixed some bugs

--- Function.py
+++ Function.py
@@ -3,16 +3,21 @@ from geopy.geocoders import Nominatim
 def add_df_to_lan_lat_geojson(df, properties):
     geojson = {'type':'FeatureCollection', 'features':[]}
     geo = Nominatim(user_agent="not-my-application")
+    find = 0
     for _, row in df.iterrows():
-        feature = {'type':'Feature',
-                   "color": "blue",
-                   'properties':{},
-                   'geometry':{'type':'Point',
-                               'coordinates':[]}}
-        corr = geo.geocode(row["pays"])
-        feature['geometry']['coordinates'] = [corr.latitude,corr.longitude]
+        lat,lon = 0,0
+        feature = {'type':'Feature', 'properties':{}, 'geometry':{'type':'Point','coordinates':[]}}        
+        try:
+            corr=geo.geocode(row["pays"])
+            lat,lon = corr.latitude,corr.longitude
+        except :
+            print("the api didnt find ", row["pays"])
+            find+=1
+            continue
+        feature['geometry']['coordinates'] = [lat,lon]
         for prop in properties:
             feature['properties'][prop] = row[prop]
         geojson['features'].append(feature)
+    print(f"didnt find {find}")
     return geojson
 
--- row_to_csv.py
+++ row_to_csv.py
@@ -1,9 +1,11 @@
 from geopy.geocoders import Nominatim
 import pandas as pd
 from Function import add_df_to_lan_lat_geojson
+import plotly.express as px
 
-properties = ["pays","cluster_km"]
-src = pd.read_csv("./data/data_with_clusters.csv")
+properties = ['pays','cluster_km', 'cluster_km_2', 'cluster_km_5']
+src = pd.read_csv("./data/data_with_clusters.csv")[properties]
 print("Wating assiging the contry coordinate...")
 geoJson = add_df_to_lan_lat_geojson(src, properties)
 print(geoJson)
+
